8f132b8a79b1cc804c034c5f6c7f12dbf39f1ae2
Luis Vejarano <luis.vejarano1.0@gmail.com>
2024-11-13 13:23:33 -0500

    Se crean SP de la tabla Permiso

--- Script_Permiso.sql
+++ Script_Permiso.sql
@@ -23,3 +23,21 @@ BEGIN
     insert into tbl_permiso(per_nombre,per_descripcion) values(p_name,p_description);
 END//
 DELIMITER ;
+
+-- Actualizar un Permiso 
+DELIMITER //
+CREATE PROCEDURE spUpdatePermission(IN p_id INT,IN p_name VARCHAR(45),IN p_description VARCHAR(80))
+BEGIN
+    update tbl_permiso
+    set per_nombre = p_name, per_descripcion = p_description
+    where per_id = p_id;
+END//
+DELIMITER ;
+
+-- Borrar un Permiso 
+DELIMITER //
+CREATE PROCEDURE spDeletePermission(IN p_id INT)
+BEGIN
+    delete from tbl_permiso where per_id = p_id;
+END//
+DELIMITER ;
